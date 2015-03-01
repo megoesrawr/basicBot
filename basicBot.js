c6636f87e6512542c5dfbdc37b4d2ac7902e4e15
CustomCraftDev <undeaD_D@custom-craft.de>
2015-03-01 13:36:19 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1,37 +1,4 @@
-
 (function () {
-
-    API.getWaitListPosition = function(id){
-        if(typeof id === 'undefined' || id === null){
-            id = API.getUser().id;
-        }
-        var wl = API.getWaitList();
-        for(var i = 0; i < wl.length; i++){
-            if(wl[i].id === id){
-                return i;
-            }
-        }
-        return -1;
-    };
-
-    var kill = function () {
-        clearInterval(basicBot.room.autodisableInterval);
-        clearInterval(basicBot.room.afkInterval);
-        basicBot.status = false;
-    };
-
-    var subChat = function (chat, obj) {
-        if (typeof chat === "undefined") {
-            API.chatLog("There is a chat text missing.");
-            console.log("There is a chat text missing.");
-            return "[Error] No text message found.";
-        }
-        var lit = '%%';
-        for (var prop in obj) {
-            chat = chat.replace(lit + prop.toUpperCase() + lit, obj[prop]);
-        }
-        return chat;
-    };
 
     var loadChat = function (cb) {
         if (!cb) cb = function () {
@@ -69,341 +36,52 @@
         });
     };
 
-    String.prototype.splitBetween = function (a, b) {
-        var self = this;
-        self = this.split(a);
-        for (var i = 0; i < self.length; i++) {
-            self[i] = self[i].split(b);
-        }
-        var arr = [];
-        for (var i = 0; i < self.length; i++) {
-            if (Array.isArray(self[i])) {
-                for (var j = 0; j < self[i].length; j++) {
-                    arr.push(self[i][j]);
-                }
-            }
-            else arr.push(self[i]);
-        }
-        return arr;
-    };
-
     var basicBot = {
-        version: "3.0",
-        status: false,
-        name: "UberBot",
-        loggedInID: null,
+        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
+        
         settings: {
-            botName: "UberBot",
+            botName: "Bukkit Bot",
             language: "english",
             chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-            startupCap: 1, // 1-200
-            startupVolume: 0, // 0-100
-            startupEmoji: false, // true or false
-            maximumAfk: 120,
-            afkRemoval: false,
-            maximumDc: 60,
-            bouncerPlus: true,
-            blacklistEnabled: false,
-            lockdownEnabled: false,
-            lockGuard: false,
-            maximumLocktime: 10,
-            cycleGuard: true,
-            maximumCycletime: 10,
-            voteSkip: false,
-            voteSkipLimit: 10,
-            timeGuard: false,
-            maximumSongLength: 10,
-            autodisable: true,
-            commandCooldown: 30,
-            usercommandsEnabled: false,
-            lockskipPosition: 3,
-            lockskipReasons: [
-                ["theme", "This song does not fit the room theme. "],
-                ["op", "This song is on the OP list. "],
-                ["history", "This song is in the history. "],
-                ["mix", "You played a mix, which is against the rules. "],
-                ["sound", "The song you played had bad sound quality or no sound. "],
-                ["nsfw", "The song you contained was NSFW (image or sound). "],
-                ["unavailable", "The song you played was not available for some users. "]
-            ],
-            afkpositionCheck: 15,
-            afkRankCheck: "ambassador",
-            motdEnabled: false,
-            motdInterval: 5,
-            motd: "Temporary Message of the Day",
-            filterChat: false,
-            etaRestriction: false,
-            welcome: false,
-            opLink: null,
-            rulesLink: null,
-            themeLink: null,
-            fbLink: null,
-            youtubeLink: null,
-            website: null,
-            intervalMessages: [],
-            messageInterval: 5,
             songstats: true,
-            commandLiteral: "***",
-            blacklists: {
-                NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
-                OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
-            }
         },
-        room: {
-            users: [],
-            afkList: [],
-            mutedUsers: [],
-            bannedUsers: [],
-            skippable: false,
-            usercommand: false,
-            allcommand: false,
-            afkInterval: null,
-            autoskip: false,
-            autoskipTimer: null,
-            autodisableInterval: null,
-            autodisableFunc: function () {
-                if (basicBot.status && basicBot.settings.autodisable) {
-                    API.sendChat('!afkdisable');
-                    API.sendChat('!joindisable');
-                }
-            },
-            queueing: 0,
-            queueable: false,
-            currentDJID: null,
-            historyList: [],
-            cycleTimer: setTimeout(function () {
-            }, 1),
-            roomstats: {
-                accountName: null,
-                totalWoots: 0,
-                totalCurates: 0,
-                totalMehs: 0,
-                launchTime: null,
-                songCount: 0,
-                chatmessages: 0
-            },
-            messages: {
-                from: [],
-                to: [],
-                message: []
-            },
-            queue: {
-                id: [],
-                position: []
-            },
-            blacklists: {
-
-            },
-            newBlacklisted: [],
-            newBlacklistedSongFunction: null,
-            roulette: {
-                rouletteStatus: false,
-                participants: [],
-                countdown: null,
-                startRoulette: function () {
-                    basicBot.room.roulette.rouletteStatus = true;
-                    basicBot.room.roulette.countdown = setTimeout(function () {
-                        basicBot.room.roulette.endRoulette();
-                    }, 60 * 1000);
-                    API.sendChat(basicBot.chat.isopen);
-                },
-                endRoulette: function () {
-                    basicBot.room.roulette.rouletteStatus = false;
-                    var ind = Math.floor(Math.random() * basicBot.room.roulette.participants.length);
-                    var winner = basicBot.room.roulette.participants[ind];
-                    basicBot.room.roulette.participants = [];
-                    var pos = Math.floor((Math.random() * API.getWaitList().length) + 1);
-                    var user = basicBot.userUtilities.lookupUser(winner);
-                    var name = user.username;
-                    API.sendChat(subChat(basicBot.chat.winnerpicked, {name: name, position: pos}));
-                    setTimeout(function (winner, pos) {
-                        basicBot.userUtilities.moveUser(winner, pos, false);
-                    }, 1 * 1000, winner, pos);
-                }
-            }
-        },
-        User: function (id, name) {
-            this.id = id;
-            this.username = name;
-            this.jointime = Date.now();
-            this.lastActivity = Date.now();
-            this.votes = {
-                woot: 0,
-                meh: 0,
-                curate: 0
-            };
-            this.lastEta = null;
-            this.afkWarningCount = 0;
-            this.afkCountdown = null;
-            this.inRoom = true;
-            this.isMuted = false;
-            this.lastDC = {
-                time: null,
-                position: null,
-                songCount: 0
-            };
-            this.lastKnownPosition = null;
-        },
-        userUtilities: {
-            getJointime: function (user) {
-                return user.jointime;
-            },
-            getUser: function (user) {
-                return API.getUser(user.id);
-            },
-            updatePosition: function (user, newPos) {
-                user.lastKnownPosition = newPos;
-            },
-            updateDC: function (user) {
-                user.lastDC.time = Date.now();
-                user.lastDC.position = user.lastKnownPosition;
-                user.lastDC.songCount = basicBot.room.roomstats.songCount;
-            },
-            setLastActivity: function (user) {
-                user.lastActivity = Date.now();
-                user.afkWarningCount = 0;
-                clearTimeout(user.afkCountdown);
-            },
-            getLastActivity: function (user) {
-                return user.lastActivity;
-            },
-            getWarningCount: function (user) {
-                return user.afkWarningCount;
-            },
-            setWarningCount: function (user, value) {
-                user.afkWarningCount = value;
-            },
-            lookupUser: function (id) {
-                for (var i = 0; i < basicBot.room.users.length; i++) {
-                    if (basicBot.room.users[i].id === id) {
-                        return basicBot.room.users[i];
-                    }
-                }
-                return false;
-            },
-            lookupUserName: function (name) {
-                for (var i = 0; i < basicBot.room.users.length; i++) {
-                    var match = basicBot.room.users[i].username.trim() == name.trim();
-                    if (match) {
-                        return basicBot.room.users[i];
-                    }
-                }
-                return false;
-            },
-            voteRatio: function (id) {
-                var user = basicBot.userUtilities.lookupUser(id);
-                var votes = user.votes;
-                if (votes.meh === 0) votes.ratio = 1;
-                else votes.ratio = (votes.woot / votes.meh).toFixed(2);
-                return votes;
-
-            }
-        },
-
-        roomUtilities: {
-            rankToNumber: function (rankString) {
-            	loadChat(API.sendChat("lol ranktonumber"));
-                var rankInt = null;
-                switch (rankString) {
-                    case "admin":
-                        rankInt = 10;
-                        break;
-                    case "ambassador":
-                        rankInt = 7;
-                        break;
-                    case "host":
-                        rankInt = 5;
-                        break;
-                    case "cohost":
-                        rankInt = 4;
-                        break;
-                    case "manager":
-                        rankInt = 3;
-                        break;
-                    case "bouncer":
-                        rankInt = 2;
-                        break;
-                    case "residentdj":
-                        rankInt = 1;
-                        break;
-                    case "user":
-                        rankInt = 0;
-                        break;
-                }
-                return rankInt;
-            }
-        },
+        
         eventDjadvance: function (obj) {
-        	loadChat(API.sendChat("lol eventdjadvance"));
-            var user = basicBot.userUtilities.lookupUser(obj.dj.id)
-            for(var i = 0; i < basicBot.room.users.length; i++){
-                if(basicBot.room.users[i].id === user.id){
-                    basicBot.room.users[i].lastDC = {
-                        time: null,
-                        position: null,
-                        songCount: 0
-                    };
-                }
-            }
-
-            if (basicBot.settings.songstats) {
-                if (typeof basicBot.chat.songstatistics === "undefined") {
-                    API.sendChat("/me " + lastplay.media.author + " - " + lastplay.media.title)
-                }
-                else {
-                    API.sendChat(subChat(basicBot.chat.songstatistics, {artist: lastplay.media.author, title: lastplay.media.title, woots: lastplay.score.positive, grabs: lastplay.score.grabs, mehs: lastplay.score.negative}))
-                }
-            }
-            basicBot.room.roomstats.totalWoots += lastplay.score.positive;
-            basicBot.room.roomstats.totalMehs += lastplay.score.negative;
-            basicBot.room.roomstats.totalCurates += lastplay.score.grabs;
-            basicBot.room.roomstats.songCount++;
-            basicBot.roomUtilities.intervalMessage();
-            basicBot.room.currentDJID = obj.dj.id;
-
-            var mid = obj.media.format + ':' + obj.media.cid;
-            for (var bl in basicBot.room.blacklists) {
-                if (basicBot.settings.blacklistEnabled) {
-                    if (basicBot.room.blacklists[bl].indexOf(mid) > -1) {
-                        API.sendChat(subChat(basicBot.chat.isblacklisted, {blacklist: bl}));
-                        return API.moderateForceSkip();
-                    }
-                }
-            }
-            if (!alreadyPlayed) {
-                basicBot.room.historyList.push([obj.media.cid, +new Date()]);
-            }
-            var newMedia = obj.media;
-            if (basicBot.settings.timeGuard && newMedia.duration > basicBot.settings.maximumSongLength * 60 && !basicBot.room.roomevent) {
-                var name = obj.dj.username;
-                API.sendChat(subChat(basicBot.chat.timelimit, {name: name, maxlength: basicBot.settings.maximumSongLength}));
-                API.moderateForceSkip();
-            }
-
+        	if (obj == null) return; // no dj
+        	
+        	var str = "";
+        	var currentDJ = obj.dj;
+        	str += currentDJ.username;
+        	        	  
+        	str += " || " + obj.media.author + " || " + obj.media.title;
+        	 
+        	API.sendChat(str);
         },
+        
         connectAPI: function () {
             this.proxy = {
-                eventChat: $.proxy(this.eventChat, this),
-                eventRoomscoreupdate: $.proxy(this.eventRoomscoreupdate, this),
-                eventDjadvance: $.proxy(this.eventDjadvance, this),
+                eventDjadvance: $.proxy(this.eventDjadvance, this)
             };
-            API.on(API.CHAT, this.proxy.eventChat);
-            API.on(API.ROOM_SCORE_UPDATE, this.proxy.eventRoomscoreupdate);
             API.on(API.ADVANCE, this.proxy.eventDjadvance);
-
+        },
+        
+        disconnectAPI: function () {
+            API.off(API.ADVANCE, this.proxy.eventDjadvance);
         },
         
         startup: function () {
+            Function.prototype.toString = function () {
+                return 'Function.'
+            };
             basicBot.connectAPI();
             window.bot = basicBot;
-            basicBot.loggedInID = API.getUser().id;
-            basicBot.status = true;
-            loadChat(API.sendChat("test"));
+
+            API.sendChat("Bot loaded successfully");
         }
-        
     };
 
     loadChat(basicBot.startup);
+    
 }).call(this);
